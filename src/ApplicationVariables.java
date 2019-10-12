de1d23c1f7245513fe7917377bb51b9959e19fe6
Maryam <maryam.aslam@outlook.com>
2019-10-12 15:17:51 +0200

    Adding support for 4th variable in sum.

--- src/ApplicationVariables.java
+++ src/ApplicationVariables.java
@@ -4,9 +4,10 @@ public class ApplicationVariables {
         int var1 = 200;
         int var2 = 400;
         int var3 = 900;
+        int var4 = 400;
         int sum;
 
-        sum = var1 + var2 + var3;
+        sum = var1 + var2 + var3 + var4;
 
         System.out.println(sum);
     }
