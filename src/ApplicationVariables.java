8bc6747bd715f6669123d5568fa604ed7c613f2d
Maryam <maryam.aslam@outlook.com>
2019-10-12 15:34:24 +0200

    Adding multiFunction

--- src/ApplicationVariables.java
+++ src/ApplicationVariables.java
@@ -6,13 +6,23 @@ public class ApplicationVariables {
         int var3 = 900;
         int var4 = 400;
         int sum;
+        int multi;
 
         sum = sumFunction(var1, var2);
+        multi = multiFunction(var1,var2);
+
 
         System.out.println(sum);
+        System.out.println(multi);
     }
 
     public static int sumFunction (int var1, int var2){
         return (var1 + var2);
     }
+
+    public static int multiFunction (int var1, int var2){
+        return (var1 * var2);
+    }
+
 }
+
