e8b771547e11214e4c36c4ff927456c8952c2173
Maryam <maryam.aslam@outlook.com>
2019-10-12 15:16:46 +0200

    Adding support for 3rd variable in sum.

--- src/ApplicationVariables.java
+++ src/ApplicationVariables.java
@@ -3,9 +3,10 @@ public class ApplicationVariables {
     public static void main(String[] args){
         int var1 = 200;
         int var2 = 400;
+        int var3 = 900;
         int sum;
 
-        sum = var1 + var2;
+        sum = var1 + var2 + var3;
 
         System.out.println(sum);
     }
