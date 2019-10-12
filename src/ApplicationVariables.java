311358f3f0cdcf4d857f9e3d077a1c1bd39e6d34
Maryam <maryam.aslam@outlook.com>
2019-10-12 15:13:07 +0200

    Initial commit

---
+++ src/ApplicationVariables.java
@@ -0,0 +1,12 @@
+public class ApplicationVariables {
+
+    public static void main(String[] args){
+        int var1 = 200;
+        int var2 = 400;
+        int sum;
+
+        sum = var1 + var2;
+
+        System.out.println(sum);
+    }
+}
